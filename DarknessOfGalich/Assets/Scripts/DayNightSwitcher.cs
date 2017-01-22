b19f866a56e5a391afb510ce02ba4a7ca15abea3
Владислав Вержбицкий < >
2017-01-22 16:00:02 +0600

    Dynamic day and night switcher

--- DarknessOfGalich/Assets/Scripts/DayNightSwitcher.cs
+++ DarknessOfGalich/Assets/Scripts/DayNightSwitcher.cs
@@ -6,32 +6,37 @@ public class DayNightSwitcher : MonoBehaviour {
 
     public GameObject light;
     public bool isDay = true;
-    public bool isNight = false;
-    public float DayLen = 10;
-    float switchTime =0 ;     
+    public float dayDuration = 60;
+    float currentTime = 0;     
     // Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time-switchTime>DayLen)
-        {
-            if (isDay)
-            {
-                isDay = false;
-                isNight = true;
-                light.GetComponent<Light>().intensity = 0.2f;
-            }
-            else
-            {
-                isDay = true;
-                isNight = false;
-                light.GetComponent<Light>().intensity = 1.8f;
-            }
-            switchTime = Time.time;
-
-        }
+        StartCoroutine("DynamicDayNight");
 	}
+
+    IEnumerator DynamicDayNight()
+    {
+        if (currentTime % (dayDuration * 2) <= dayDuration)
+        {
+            isDay = true;
+        } else
+        {
+            isDay = false;
+        }
+        if (isDay)
+        {
+            float coef = Mathf.Abs(dayDuration / 2 - currentTime % (dayDuration * 2));
+            light.GetComponent<Light>().intensity = 1.8f - 2.4f * coef / dayDuration;
+        } else
+        {
+            float coef = Mathf.Abs(dayDuration * 3 / 2 - currentTime % (dayDuration * 2));
+            light.GetComponent<Light>().intensity = 1.2f * coef / dayDuration;
+        }
+        currentTime = Time.time;
+        yield return null;
+    }
 }
